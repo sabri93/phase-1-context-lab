00e3b857a67acf9e2d925dbf911e7af878aa89f0
sabrina <sabrinadjebbar93@gmail.com>
2022-05-22 20:43:59 -0400

    completed assignment

--- index.js
+++ index.js
@@ -21,3 +21,62 @@ const allWagesFor = function () {
     return payable
 }
 
+const createEmployeeRecord=([firstName, familyName,title, payPerHour])=>{
+   
+    return {
+    firstName  ,
+    familyName  ,
+    title  ,
+    payPerHour ,
+    timeInEvents : [] ,
+    timeOutEvents : []
+   }
+   
+}
+
+const createEmployeeRecords = (employeeRecords)=>{
+   const result = []
+       for(const employee of employeeRecords){
+           result.push(createEmployeeRecord(employee));
+
+       }
+   
+    return result 
+
+    
+}
+
+ const createTimeInEvent = (timeStamp)=>{
+    const timeObj ={
+        type : "TimeIn",
+        date : timeStamp.split(" ")[0],
+        hour: parseInt(timeStamp.slice(-4), 10)
+    }
+   record.timeInEvents.push(timeObj)
+   return record
+ }
+
+
+ const createTimeOutEvent = (record,timeStamp)=>{
+    const timeObj ={
+        type : "TimeOut",
+        date : timeStamp.split(" ")[0],
+        hour: parseInt(timeStamp.slice(-4), 10)
+    }
+   record.timeOutEvents.push(timeObj)
+   return record
+ }
+
+const hoursWorkedOnDate = (record, date) =>{
+   return (record.timeOutEvents[0].hour - record.timeInEvents[0].hour)/100
+}
+
+
+const wagesEarnedOnDate = (record, date) => {
+  let pay = 0
+  const hours = hoursWorkedOnDate(record, date)
+  pay += hours*record.payPerHour
+  return pay
+}
+
+
